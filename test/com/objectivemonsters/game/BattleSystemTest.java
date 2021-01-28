ed435471b60767b0cebc986039a2f392dce5b39d
Cody Cronan <codycronberger@gmail.com>
2021-01-28 14:13:00 -0800

    Added tests for attackDamage method

---
+++ test/com/objectivemonsters/game/BattleSystemTest.java
@@ -0,0 +1,17 @@
+package com.objectivemonsters.game;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BattleSystemTest {
+
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void attackDamage() {
+    }
+}
