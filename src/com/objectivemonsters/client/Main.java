9a43b44fa5bb97e49c191b64e22ed4d936271b5f
Cody Cronan <codycronberger@gmail.com>
2021-01-28 11:18:52 -0800

    skeleton of BattleSystem V1

--- src/com/objectivemonsters/client/Main.java
+++ src/com/objectivemonsters/client/Main.java
@@ -7,5 +7,9 @@ public class Main {
     public static void main(String[] args) {
         TextParser tp = new TextParser();
         tp.monsterXML();
+//        List<String> validVerbs = List.of("GO", "OPEN", "INSPECT", "LOOK", "KEEP", "DROP", "FIGHT");
+//        Prompter prompter = new Prompter(new Scanner(System.in));
+//        String response = prompter.prompt("Where would you like to go?", validVerbs, "That is not a valid command");
+//        System.out.println("You have chosen to " + response);
     }
 }
