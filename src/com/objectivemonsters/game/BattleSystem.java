aa538cb435a069ee7b03a7ed1d102f2225632170
Cody Cronan <codycronberger@gmail.com>
2021-01-29 08:31:18 -0800

    added messages to the battle method to simulate a fight for the user to view

--- src/com/objectivemonsters/game/BattleSystem.java
+++ src/com/objectivemonsters/game/BattleSystem.java
@@ -52,6 +52,7 @@ public class BattleSystem {
  * Monster m1 attacks, damage removed from m2 hp, new hp set for m2
  * Monster m2 attacks, damage removed from m1 hp, new hp set for m1
  * Just getting all mechanics working before making more usable
+ * version 1 - use Thread.sleep to control flow of battle, and use simple println statements to visualize fight
  */
     public Monster battle(Monster m1, Monster m2) {
         //battle logic here
@@ -60,16 +61,51 @@ public class BattleSystem {
             int m1AtckDmg = attackDamage(m1.getStrength());
             int m2DmgHp = m2.getHP() - m1AtckDmg;
             m2.setHP(m2DmgHp);
+            // check if m2 is dead
             if (m2.getHP() <= 0) {
+                //m2 hp = 0
+                printBattleMessage(m1.getName() + " charges out and slashes " + m2.getName() + " accross the face in a powerful attack that caused " + m1AtckDmg + " dmg.",m2.getName() + " lets out a cry while collapsing to the ground dead");
                 break;
             }
+            //m2 hit message
+            printBattleMessage(m1.getName() + " charges out and slashes " + m2.getName() + " accross the face in a powerful attack that caused " + m1AtckDmg + " dmg.",m2.getName() + " stumbles back from the attack, but begins to regain its balance");
+            //sleep to slow down output
+            delayBattleMessages(4000);
             //m2 takes turn
             int m2AtckDmg = attackDamage(m2.getStrength());
             int m1DmgHp = m1.getHP() - m2AtckDmg;
             m1.setHP(m1DmgHp);
+            //check if m1 is dead
+            if (m1.getHP() <= 0) {
+                //m1 hp = 0
+                printBattleMessage(m2.getName() + " dashes toward " + m1.getName() + " and swings its arms, slamming into " + m1.getName() + " causing " + m2AtckDmg + " dmg.",m1.getName() + " falls to the ground, trying to get back up, but finally collapses to the floor for the last time");
+                break;
+            }
+            //m1 hit message
+            printBattleMessage(m2.getName() + " dashes toward " + m1.getName() + " and swings its arms, slamming into " + m1.getName() + " causing " + m2AtckDmg + " dmg.",m1.getName() + " falls back wincing in pain, but begins to regain its composure");
+            //sleep to slow down output of battle
+            delayBattleMessages(4000);
         }
 
         return m1.getHP() > 0 ? m1 : m2; //return the monster that won the battle
+    }
+
+
+    //prints the attack message, delays, then prints the hit message
+    private void printBattleMessage(String attackMessage, String hitMessage) {
+        System.out.println(attackMessage);
+        delayBattleMessages(2000);
+        System.out.println(hitMessage + "\n");
+    }
+
+    //Method to slow down the output and avoid bombarding the user with text
+    //help to simulate an actual battle
+    private void delayBattleMessages(int timeDelay) {
+        try {
+            Thread.sleep(timeDelay);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     /*
