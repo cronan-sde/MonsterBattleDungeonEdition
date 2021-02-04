7f0ea47b16e19e5738e173c2133407f074fdd592
Cody Cronan <codycronberger@gmail.com>
2021-02-04 15:40:11 -0800

    showing shards

--- src/com/objectivemonsters/game/GameGUI.java
+++ src/com/objectivemonsters/game/GameGUI.java
@@ -191,7 +191,7 @@ public class GameGUI extends JFrame implements KeyListener {
 
         userMonsters.setText("MONSTERS:" + playerMonstersLabel());
         userInventory.setText("Inventory:" + player.getpItems());
-        userShards.setText("Shards " + player.getpShards().size() + "/10: " + player.getpShards());
+        userShards.setText("Shards " + player.getpShards().size() + "/10: ");
         userKeys.setText("Keys " + player.getKey(player.getpShards()) + "/1" + ": [" + player.getKey(player.getpShards())+"]"); //TODO: figure out key, currently only 1 key per level, shards morph into key
     }
 
