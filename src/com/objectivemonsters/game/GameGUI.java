3d2146b69a43aaab3d31fc70044d4a45a9230c5b
Cody Cronan <codycronberger@gmail.com>
2021-02-05 13:51:03 -0800

    removed unused variables, added method for random chance to miss in battle

--- src/com/objectivemonsters/game/GameGUI.java
+++ src/com/objectivemonsters/game/GameGUI.java
@@ -44,7 +44,6 @@ public class GameGUI extends JFrame implements KeyListener {
     private Player player;
     private Dungeon dungeon;
     private GameController controller;
-    private MonsterList monsterList;
 
     //testing scenes
     private StartScene startScene;
@@ -203,8 +202,8 @@ public class GameGUI extends JFrame implements KeyListener {
 
         userMonsters.setText("MONSTERS:" + playerMonstersLabel());
         userInventory.setText("Inventory:" + player.getpItems());
-        userShards.setText("Shards [" + player.getpShards().size() + "/10:]");
-        userKeys.setText("Keys [" + player.getKey(player.getpShards()) + "/1]"); //TODO: figure out key, currently only 1 key per level, shards morph into key
+        userShards.setText("Shards: " + player.getpShards().size() + "/10");
+        userKeys.setText("Keys: " + player.getKey(player.getpShards()) + "/1"); //TODO: figure out key, currently only 1 key per level, shards morph into key
         
     }
 
