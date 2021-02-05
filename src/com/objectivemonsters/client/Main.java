eb22d82d694c522620a2d9bd96eed934a648f0b0
Cody Cronan <codycronberger@gmail.com>
2021-02-05 09:42:17 -0800

    moved start code to client Main

--- src/com/objectivemonsters/client/Main.java
+++ src/com/objectivemonsters/client/Main.java
@@ -1,19 +1,10 @@
 package com.objectivemonsters.client;
-import com.objectivemonsters.map.Dungeon;
-import com.objectivemonsters.game.GameController;
-import com.objectivemonsters.map.MapInit;
-import com.objectivemonsters.monsters.Monster;
-import com.objectivemonsters.monsters.MonsterGenerator;
-import com.objectivemonsters.monsters.MonsterList;
-import com.objectivemonsters.player.Player;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.objectivemonsters.game.GameGUI;
 
 public class Main {
 
-//    public static void main(String[] args) {
-//        GameController gc = new GameController();
-//        gc.startGame();
-//    }
+    public static void main(String[] args) {
+        new GameGUI();
+    }
 }
